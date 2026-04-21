5c9ce132a4bed648f0114ca6b06f61c976dad22e
mhwrexwork <mhwrexwork@gmail.com>
2026-04-21 22:29:56 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -10,44 +10,44 @@ import io
 
 
 # --- Configuration Parameters (from notebook) ---
-characters = sorted(['D', 't', 'e', 'G', 'n', 'W', 'w', 'm', 'h', 'z', 'o', 'V', '6', 'J', 'v', '9', 's', 'H', 'Q', 'S', '5', 'i', 'C', 'f', 'u', 'B', 'g', 'd', 'q', 'j', 'l', 'Y', 'r', '3', '2', 'T', '8', '0', '1', 'P', 'X', 'c', 'b', 'Z', 'U', 'a', 'x', 'N', 'k', 'F', 'O', 'I', 'p', '7', 'E', 'L', 'K', 'R', 'y', '4', 'A', 'M'])
-img_width = 200
-img_height = 50
-downsample_factor = 4
+#characters = sorted(['D', 't', 'e', 'G', 'n', 'W', 'w', 'm', 'h', 'z', 'o', 'V', '6', 'J', 'v', '9', 's', 'H', 'Q', 'S', '5', 'i', 'C', 'f', 'u', 'B', 'g', 'd', 'q', 'j', 'l', 'Y', 'r', '3', '2', 'T', '8', '0', '1', 'P', 'X', 'c', 'b', 'Z', 'U', 'a', 'x', 'N', 'k', 'F', 'O', 'I', 'p', '7', 'E', 'L', 'K', 'R', 'y', '4', 'A', 'M'])
+#img_width = 200
+#img_height = 50
+#downsample_factor = 4
 
 # --- Custom CTC Layer (required for loading the model) ---
-class CTCLayer(layers.Layer):
-  def __init__(self, name=None, **kwargs):
-    super().__init__(name=name, **kwargs)
-    self.loss_fn = keras.backend.ctc_batch_cost
+#class CTCLayer(layers.Layer):
+  #def __init__(self, name=None, **kwargs):
+    #super().__init__(name=name, **kwargs)
+    #self.loss_fn = keras.backend.ctc_batch_cost
 
-  def call(self, y_true, y_pred):
-    batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
-    input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
-    label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
+  #def call(self, y_true, y_pred):
+    #batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
+    #input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
+    #label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
 
-    input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-    label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+    #input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+    #label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
 
-    loss = self.loss_fn(y_true, y_pred, input_length, label_length)
-    self.add_loss(loss)
-    return y_pred
+    #loss = self.loss_fn(y_true, y_pred, input_length, label_length)
+    #self.add_loss(loss)
+    #return y_pred
 
 # --- Character Mappers (recreated from configuration) ---
-char_to_num = layers.StringLookup(vocabulary=list(characters), mask_token=None)
-num_to_char = layers.StringLookup(
-    vocabulary=char_to_num.get_vocabulary(), mask_token=None, invert=True
-)
+#char_to_num = layers.StringLookup(vocabulary=list(characters), mask_token=None)
+#num_to_char = layers.StringLookup(
+    #vocabulary=char_to_num.get_vocabulary(), mask_token=None, invert=True
+#)
 
 # --- Prediction Decoding Function ---
-def decode_batch_predictions(pred):
-    input_len = ops.ones(pred.shape[0]) * pred.shape[1]
-    results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0]
-    output_text = []
-    for res in results:
-        res = tf.gather(res, tf.where(tf.math.not_equal(res, -1)))
-        output_text.append(tf.strings.reduce_join(num_to_char(res)).numpy().decode("utf-8"))
-    return output_text
+#def decode_batch_predictions(pred):
+    #input_len = ops.ones(pred.shape[0]) * pred.shape[1]
+    #results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0]
+    #output_text = []
+    #for res in results:
+        #res = tf.gather(res, tf.where(tf.math.not_equal(res, -1)))
+        #output_text.append(tf.strings.reduce_join(num_to_char(res)).numpy().decode("utf-8"))
+    #return output_text
 
 # --- Load the Model ---
 @st.cache_resource
