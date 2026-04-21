ed72e7dcf4d5cf6cbc7287e1dc2b253b3ab4433a
mhwrexwork <mhwrexwork@gmail.com>
2026-04-21 22:40:57 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import tensorflow as tf
-#from tensorflow import keras
 import keras
 from keras import layers
 from keras import ops
@@ -10,44 +9,42 @@ import io
 
 
 # --- Configuration Parameters (from notebook) ---
-#characters = sorted(['D', 't', 'e', 'G', 'n', 'W', 'w', 'm', 'h', 'z', 'o', 'V', '6', 'J', 'v', '9', 's', 'H', 'Q', 'S', '5', 'i', 'C', 'f', 'u', 'B', 'g', 'd', 'q', 'j', 'l', 'Y', 'r', '3', '2', 'T', '8', '0', '1', 'P', 'X', 'c', 'b', 'Z', 'U', 'a', 'x', 'N', 'k', 'F', 'O', 'I', 'p', '7', 'E', 'L', 'K', 'R', 'y', '4', 'A', 'M'])
-#img_width = 200
-#img_height = 50
-#downsample_factor = 4
+characters = sorted(['F', '6', '1', 'B', 'a', 'L', 'm', '2', '7', 'k', 'c', 'v', 'p', 'z', '4', 'N', 'O', 'I', 'y', 'g', 'P', '3', 'K', 'V', 'E', 'U', 'T', 'j', 'Q', 'J', 'l', '0', 'r', 'i', 's', 'n', 'M', 'W', 'q', 'S', 'G', 'f', 'x', 'e', 't', 'd', 'Y', 'A', 'R', 'w', 'b', '5', 'h', 'o', 'H', '8', 'u', 'Z', 'C', 'X', 'D', '9'])
+img_width = 200
+img_height = 50
+downsample_factor = 4
 
 # --- Custom CTC Layer (required for loading the model) ---
-#class CTCLayer(layers.Layer):
-  #def __init__(self, name=None, **kwargs):
-    #super().__init__(name=name, **kwargs)
-    #self.loss_fn = keras.backend.ctc_batch_cost
+@keras.saving.register_keras_serializable()
+class CTCLayer(layers.Layer):
+  def __init__(self, name=None, **kwargs):
+    super().__init__(name=name, **kwargs)
+    self.loss_fn = keras.ops.ctc_loss
 
-  #def call(self, y_true, y_pred):
-    #batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
-    #input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
-    #label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
+  def call(self, y_true, y_pred):
+    batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
+    input_length = tf.fill([batch_len], tf.cast(tf.shape(y_pred)[1], dtype="int64"))
+    label_length = tf.fill([batch_len], tf.cast(tf.shape(y_true)[1], dtype="int64"))
 
-    #input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-    #label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-
-    #loss = self.loss_fn(y_true, y_pred, input_length, label_length)
-    #self.add_loss(loss)
-    #return y_pred
+    loss = self.loss_fn(y_true, y_pred, input_length, label_length)
+    self.add_loss(loss)
+    return y_pred
 
 # --- Character Mappers (recreated from configuration) ---
-#char_to_num = layers.StringLookup(vocabulary=list(characters), mask_token=None)
-#num_to_char = layers.StringLookup(
-    #vocabulary=char_to_num.get_vocabulary(), mask_token=None, invert=True
-#)
+char_to_num = layers.StringLookup(vocabulary=list(characters), mask_token=None)
+num_to_char = layers.StringLookup(
+    vocabulary=char_to_num.get_vocabulary(), mask_token=None, invert=True
+)
 
 # --- Prediction Decoding Function ---
-#def decode_batch_predictions(pred):
-    #input_len = ops.ones(pred.shape[0]) * pred.shape[1]
-    #results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0]
-    #output_text = []
-    #for res in results:
-        #res = tf.gather(res, tf.where(tf.math.not_equal(res, -1)))
-        #output_text.append(tf.strings.reduce_join(num_to_char(res)).numpy().decode("utf-8"))
-    #return output_text
+def decode_batch_predictions(pred):
+    input_len = ops.ones(pred.shape[0]) * pred.shape[1]
+    results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0]
+    output_text = []
+    for res in results:
+        res = tf.gather(res, tf.where(tf.math.not_equal(res, -1)))
+        output_text.append(tf.strings.reduce_join(num_to_char(res)).numpy().decode("utf-8"))
+    return output_text
 
 # --- Load the Model ---
 @st.cache_resource
@@ -57,21 +54,12 @@ def load_model():
     )
     # Create a prediction model from the loaded full model
     prediction_model = keras.models.Model(
-        inputs=model.get_layer(name="image").input,
+        inputs=model.get_layer(name="image").output, # Changed to .output to get the KerasTensor
         outputs=model.get_layer(name="dense2").output
     )
     return prediction_model
 
 prediction_model = load_model()
-
-# --- Image Preprocessing Function ---
-def preprocess_image(image):
-    img = tf.io.decode_png(tf.io.read_file(image), channels=1) # This line expects bytes or path
-    img = tf.image.convert_image_dtype(img, tf.float32)
-    img = ops.image.resize(img, [img_height, img_width])
-    img = ops.transpose(img, axes=[1, 0, 2])
-    img = tf.expand_dims(img, axis=0)  # Add batch dimension
-    return img
 
 # --- Streamlit App Layout ---
 st.set_page_config(layout="centered", page_title="Captcha OCR Predictor")
